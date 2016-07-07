a3b8d803e9260147b23eb0dcda71aafc12ba8bbe
MCJack123 <jackbruienne@yahoo.com>
2016-07-07 18:04:04 -0400

    .dat extensions are pointless
    .dat is not meant for this usage!!! It is for program resources, not game saves and dummy files! (I'm using .ags for AdventureGameSave, also used with AfterGRASP and AppGini)

--- game_main.py
+++ game_main.py
@@ -4,22 +4,22 @@ import time
 import random
 
 try:
-    already_ran = open("already_ran.dat","r")
+    already_ran = open(".done","r")
     already_ran.close()
 except:
     runpy.run_path("about.py")
     #run about.py
     time.sleep(1)
-    already_ran = open("already_ran.dat","w")
+    already_ran = open(".done","w")
     already_ran.write("Already Ran!!!")
     already_ran.close()
 
 #try:
 if True:
-    save = open("game_save.dat","r")
+    save = open("game_save.ags","r")
     save.close()
     save_array = []
-    with open('game_save.dat', 'r') as f:
+    with open('game_save.ags', 'r') as f:
         save_array.append(f.readline().strip())
     print("Continue Save: " + save_array[0] + "?")
     continue_save = "INVALID"
