c84557c639e83a12a376be98f4177ad431110a9b
ealgase <mostdigitsofpi@gmail.com>
2016-07-09 19:54:48 -0400

    add gitignore and make major changes to loading saves

--- game_main.py
+++ game_main.py
@@ -14,16 +14,42 @@ except:
     already_ran.write("Already Ran!!!")
     already_ran.close()
 
-#try:
-if True:
-    save = open("game_save.ags","r")
-    save.close()
+try:
+    save_test = open("game_save.ags","r")
+    save_test.close()
+    save_avaliable = 1
+except:
+    save_avaliable = 0
+if save_avaliable == 1:
     save_array = []
     with open('game_save.ags', 'r') as f:
-        save_array.append(f.readline().strip())
+        for line in f:
+            save_array.append(line.strip())
+    save_array_items = []
+    with open('items.ags', 'r') as f:
+        for line in f:
+            save_array_items.append(line.strip())
     print("Continue Save: " + save_array[0] + "?")
     continue_save = "INVALID"
     while continue_save != "Y" and continue_save != "y" and continue_save != "N" and continue_save != "n":
         continue_save=input("Y/n?")
-#except:
+    if continue_save=="y" or continue_save=="Y":
+        print("Loading save")
+        world_name=save_array[0]
+        print("world_name="+save_array[0])
+        gold=int(save_array[1])
+        print("gold="+save_array[1])
+        lives=int(save_array[2])
+        print("lives="+save_array[2])
+        xp=int(save_array[3])
+        print("xp="+save_array[3])
+        armor=int(save_array[4])
+        print("armor="+save_array[4])
+        attack_multiplier=int(save_array[5])
+        print("attack_multiplier="+save_array[5])
+        bonus_health=int(save_array[6])
+        print("bonus_health="+save_array[6])
+        items=save_array_items
+        print("items="+str(save_array_items))
+#else:
 #    pass
